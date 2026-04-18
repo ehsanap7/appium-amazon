ae7f56cc7a8fa44e0c30102dbac6bfaeb3f89253
MohammadEhsan Akhavanpour <akhavanpour.ehsan@gmail.com>
2026-04-18 19:06:15 -0400

    Allure Report version 1. #4

--- src/test/java/com/seamley/amazon/android/runner/CucumberRunner.java
+++ src/test/java/com/seamley/amazon/android/runner/CucumberRunner.java
@@ -18,6 +18,7 @@ import org.testng.annotations.DataProvider;
         objectFactory = PicoFactory.class,
         plugin = {
                 "pretty",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "html:target/cucumber-reports/cucumber.html",
                 "json:target/cucumber-reports/cucumber.json"
         },
